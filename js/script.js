d0cff269c575f9f04459792fb3c4828dd2bd3ce9
平松慶太 <hiramatsukeita@hiramatsukeitanoMacBook-Air.local>
2026-05-28 10:38:49 +0900

    serviceとnews完成

--- js/script.js
+++ js/script.js
@@ -2,27 +2,30 @@ $(function () {
   // ハンバーガーメニュー
   $('.js-btn-menu').on('click', function () {
     $(this).toggleClass('is-active');
-    $('.p-header__global-nav').toggleClass('is-active');
+    $('.p-header__nav-wrap').toggleClass('is-active');
   });
-  const worksSwiper = new Swiper('.p-works__slider', {
+  if (document.querySelector('.p-works__slider')) {
 
-    loop: true,
+    const worksSwiper = new Swiper('.p-works__slider', {
 
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-    },
+      loop: true,
 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
 
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  }
 
   // ヘッダースクロール後に背景画像出現
   $(function () {
@@ -55,5 +58,63 @@ $(function () {
       $('body, html').animate({ scrollTop: 0 }, 500);
     });
   });
+
+});
+
+/**
+ * SNS Share URL生成
+ * 実務では「URL生成責務」を
+ * HTMLから分離するのが重要
+ */
+
+const SHARE_URL = {
+  facebook: 'https://www.facebook.com/sharer/sharer.php',
+
+  x: 'https://twitter.com/share',
+
+  hatena: 'https://b.hatena.ne.jp/entry/',
+
+  line: 'https://line.me/R/msg/text/',
+};
+
+/**
+ * 現在ページ情報
+ */
+const currentUrl = encodeURIComponent(location.href);
+
+const currentTitle = encodeURIComponent(document.title);
+
+/**
+ * SNS別URL生成
+ */
+function createShareUrl(type) {
+
+  switch (type) {
+
+    case 'facebook':
+      return `${SHARE_URL.facebook}?u=${currentUrl}`;
+
+    case 'x':
+      return `${SHARE_URL.x}?url=${currentUrl}&text=${currentTitle}`;
+
+    case 'hatena':
+      return `${SHARE_URL.hatena}${currentUrl}`;
+
+    case 'line':
+      return `${SHARE_URL.line}?${currentTitle}%0A${currentUrl}`;
+
+    default:
+      return '#';
+  }
+}
+
+/**
+ * 各シェアリンクへURLセット
+ */
+document.querySelectorAll('.js-share').forEach((button) => {
+
+  const type = button.dataset.share;
+
+  button.href = createShareUrl(type);
 
 });
